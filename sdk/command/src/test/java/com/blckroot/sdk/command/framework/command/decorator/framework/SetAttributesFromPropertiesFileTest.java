6c0eda0cd8daf687d24c11a690557754534e9b62
Jesse Sandvik <sandvikjesse@gmail.com>
2023-11-19 11:43:21 -0500

    expanded test coverage, multiple positional parameters, first index option

--- sdk/command/src/test/java/com/blckroot/sdk/command/framework/command/decorator/framework/SetAttributesFromPropertiesFileTest.java
+++ sdk/command/src/test/java/com/blckroot/sdk/command/framework/command/decorator/framework/SetAttributesFromPropertiesFileTest.java
@@ -13,6 +13,8 @@ public class SetAttributesFromPropertiesFileTest {
     private final String VALID_PROPERTIES_DIRECTORY = "src/test/resources/";
     private final String VALID_PROPERTIES_FILE_PATH = VALID_PROPERTIES_DIRECTORY + "test.properties";
 
+    // Command Model Attributes
+
     @Test
     void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__null() throws Exception {
         FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
@@ -98,8 +100,23 @@ public class SetAttributesFromPropertiesFileTest {
         assertEquals(expected, actual);
     }
 
+    // Framework Command Positional Parameters
+
     @Test
-    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__label() throws Exception {
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter_count() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        int expected = Integer.parseInt(properties.getProperty("positional.parameter.count"));
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        int actual = command.getPositionalParameters().size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__label__first_index() throws Exception {
         FileSystemService fileSystemService = new FileSystemService();
         Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
         String expected = properties.getProperty("1.positional.parameter.label");
@@ -112,7 +129,7 @@ public class SetAttributesFromPropertiesFileTest {
     }
 
     @Test
-    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__synopsis() throws Exception {
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__synopsis__first_index() throws Exception {
         FileSystemService fileSystemService = new FileSystemService();
         Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
         String expected = properties.getProperty("1.positional.parameter.synopsis");
@@ -125,7 +142,7 @@ public class SetAttributesFromPropertiesFileTest {
     }
 
     @Test
-    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__value__string() throws Exception {
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__value__string__first_index() throws Exception {
         FileSystemService fileSystemService = new FileSystemService();
         Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
         String expected = properties.getProperty("1.positional.parameter.value");
@@ -134,6 +151,125 @@ public class SetAttributesFromPropertiesFileTest {
                 new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
         command.call();
         String actual = (String) command.getPositionalParameters().get(0).getValue();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__label__second_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        String expected = properties.getProperty("2.positional.parameter.label");
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        String actual = command.getPositionalParameters().get(1).getLabel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__synopsis__second_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        String expected = properties.getProperty("2.positional.parameter.synopsis");
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        String actual = command.getPositionalParameters().get(1).getSynopsis();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__positional_parameter__value__boolean__second_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        boolean expected = Boolean.parseBoolean(properties.getProperty("2.positional.parameter.value"));
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        boolean actual = !command.getPositionalParameters().get(1).getValue().toString().isBlank();
+        assertEquals(expected, actual);
+    }
+
+    // Framework Command Options
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__option_count() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        int expected = Integer.parseInt(properties.getProperty("option.count"));
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        int actual = command.getOptions().size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__option_long_name__first_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        String expected = properties.getProperty("1.option.long.name");
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        String actual = command.getOptions().get(0).getLongName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__option_short_name__first_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        String expected = properties.getProperty("1.option.short.name");
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        String actual = command.getOptions().get(0).getShortName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__option_synopsis__first_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        String expected = properties.getProperty("1.option.synopsis");
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        String actual = command.getOptions().get(0).getSynopsis();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__option_label__first_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        String expected = properties.getProperty("1.option.label");
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        String actual = command.getOptions().get(0).getLabel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void SET_ATTRIBUTES_FROM_PROPERTIES_FILE__valid__option_value__string__first_index() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        Properties properties = fileSystemService.getPropertiesFromFile(VALID_PROPERTIES_FILE_PATH);
+        String expected = properties.getProperty("1.option.value");
+
+        FrameworkBaseCommand command = new SetAttributesFromPropertiesFile(
+                new FrameworkCommand("test"), VALID_PROPERTIES_DIRECTORY);
+        command.call();
+        String actual = command.getOptions().get(0).getValue().toString();
         assertEquals(expected, actual);
     }
 }
