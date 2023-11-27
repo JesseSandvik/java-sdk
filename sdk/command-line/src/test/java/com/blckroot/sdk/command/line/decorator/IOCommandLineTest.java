02a5f8b7fcc39a35f7436b05a4dc4442ce844a27
Jesse Sandvik <sandvikjesse@gmail.com>
2023-11-26 23:17:38 -0500

    improving test coverage for IOCommandLine, currently at 91%

--- sdk/command-line/src/test/java/com/blckroot/sdk/command/line/decorator/IOCommandLineTest.java
+++ sdk/command-line/src/test/java/com/blckroot/sdk/command/line/decorator/IOCommandLineTest.java
@@ -70,6 +70,194 @@ public class IOCommandLineTest {
     // BASE COMMAND
 
     @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__exit_code__success() throws Exception {
+        int expected = 0;
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        int actual = commandLine.execute(new String[]{});
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_name() throws Exception {
+        String expected = baseCommand.getName();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(expected));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_version__short() throws Exception {
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains("-v"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_version__long() throws Exception {
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains("--version"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_version__short() throws Exception {
+        baseCommand.setVersion(baseCommandVersion);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains("-v"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_version__long() throws Exception {
+        baseCommand.setVersion(baseCommandVersion);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains("--version"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_positional_parameter__label() throws Exception {
+        PositionalParameter positionalParameter = getPositionalParameter();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains(positionalParameter.getLabel()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_positional_parameter__label() throws Exception {
+        PositionalParameter positionalParameter = getPositionalParameter();
+        baseCommand.addPositionalParameter(positionalParameter);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(positionalParameter.getLabel()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_positional_parameter__synopsis() throws Exception {
+        PositionalParameter positionalParameter = getPositionalParameter();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains(positionalParameter.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_positional_parameter__synopsis() throws Exception {
+        PositionalParameter positionalParameter = getPositionalParameter();
+        baseCommand.addPositionalParameter(positionalParameter);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(positionalParameter.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_option__long_name() throws Exception {
+        Option option = getOption();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains(option.getLongName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_option__long_name() throws Exception {
+        Option option = getOption();
+        baseCommand.addOption(option);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(option.getLongName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_option__synopsis() throws Exception {
+        Option option = getOption();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains(option.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_option__synopsis() throws Exception {
+        Option option = getOption();
+        baseCommand.addOption(option);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(option.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_option__short_name() throws Exception {
+        Option option = getOption();
+        addOptionShortName(option);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains(option.getShortName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_option__short_name() throws Exception {
+        Option option = getOption();
+        addOptionShortName(option);
+        baseCommand.addOption(option);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(option.getShortName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_option__label() throws Exception {
+        Option option = getOption();
+        addOptionLabel(option);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains(option.getLabel()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_option__label() throws Exception {
+        Option option = getOption();
+        addOptionLabel(option);
+        baseCommand.addOption(option);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(option.getLabel()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_no_subcommand__name() throws Exception {
+        Command subcommand = getSubcommand();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertFalse(out.toString().contains(subcommand.getName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__usage_help__no_arguments__output__command_subcommand__name() throws Exception {
+        Command subcommand = getSubcommand();
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{});
+
+        assertTrue(out.toString().contains(subcommand.getName()));
+    }
+
+    @Test
     void IO_COMMAND_LINE__usage_help__short_option__exit_code__success() throws Exception {
         int expected = 0;
         CommandLine commandLine = new IOCommandLine(getCommandLineCore());
@@ -522,6 +710,218 @@ public class IOCommandLineTest {
     }
 
     // SUBCOMMAND
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__exit_code__success() throws Exception {
+        int expected = 0;
+        Command subcommand = getSubcommand();
+        baseCommand.addSubcommand(subcommand);
+
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        int actual = commandLine.execute(new String[]{subcommand.getName()});
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_name() throws Exception {
+        Command subcommand = getSubcommand();
+        baseCommand.addSubcommand(subcommand);
+
+        String expected = subcommand.getName();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{expected});
+
+        assertTrue(out.toString().contains(expected));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_version__short() throws Exception {
+        Command subcommand = getSubcommand();
+        baseCommand.addSubcommand(subcommand);
+
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains("-v"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_version__long() throws Exception {
+        Command subcommand = getSubcommand();
+        baseCommand.addSubcommand(subcommand);
+
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains("--version"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_version__short() throws Exception {
+        Command subcommand = getSubcommand();
+        subcommand.setVersion(baseCommandVersion);
+        baseCommand.addSubcommand(subcommand);
+
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains("-v"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_version__long() throws Exception {
+        Command subcommand = getSubcommand();
+        subcommand.setVersion(baseCommandVersion);
+        baseCommand.addSubcommand(subcommand);
+
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains("--version"));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_positional_parameter__label() throws Exception {
+        Command subcommand = getSubcommand();
+        baseCommand.addSubcommand(subcommand);
+
+        PositionalParameter positionalParameter = getPositionalParameter();
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains(positionalParameter.getLabel()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_positional_parameter__label() throws Exception {
+        Command subcommand = getSubcommand();
+        PositionalParameter positionalParameter = getPositionalParameter();
+        subcommand.addPositionalParameter(positionalParameter);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains(positionalParameter.getLabel()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_positional_parameter__synopsis() throws Exception {
+        Command subcommand = getSubcommand();
+        PositionalParameter positionalParameter = getPositionalParameter();
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains(positionalParameter.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_positional_parameter__synopsis() throws Exception {
+        Command subcommand = getSubcommand();
+        PositionalParameter positionalParameter = getPositionalParameter();
+        subcommand.addPositionalParameter(positionalParameter);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains(positionalParameter.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_option__long_name() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains(option.getLongName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_option__long_name() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        subcommand.addOption(option);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains(option.getLongName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_option__synopsis() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains(option.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_option__synopsis() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        subcommand.addOption(option);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains(option.getSynopsis()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_option__short_name() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        addOptionShortName(option);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains(option.getShortName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_option__short_name() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        addOptionShortName(option);
+        subcommand.addOption(option);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains(option.getShortName()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_no_option__label() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        addOptionLabel(option);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertFalse(out.toString().contains(option.getLabel()));
+    }
+
+    @Test
+    void IO_COMMAND_LINE__subcommand__usage_help__no_arguments__output__command_option__label() throws Exception {
+        Command subcommand = getSubcommand();
+        Option option = getOption();
+        addOptionLabel(option);
+        subcommand.addOption(option);
+        baseCommand.addSubcommand(subcommand);
+        CommandLine commandLine = new IOCommandLine(getCommandLineCore());
+        commandLine.execute(new String[]{subcommand.getName()});
+
+        assertTrue(out.toString().contains(option.getLabel()));
+    }
 
     @Test
     void IO_COMMAND_LINE__subcommand__usage_help__short_option__exit_code__success() throws Exception {
